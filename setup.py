80918baf80878affbdf6eccdec91bd8902214806
Tyler Kennedy <tk@tkte.ch>
2014-09-09 23:03:59 -0400

    PEP8 setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
 from distutils.core import setup
 
-setup (
-    name = 'reddit2Kindle',
-    scripts = ['r2K'],
-    packages = ['redditKindleLib'],
-    version = '0.5.0',
-    author = 'Antriksh Yadav',
-    author_email = 'antrikshy@gmail.com',
-    url = 'http://www.antrikshy.com/projects/reddit2Kindle.htm',
-    description = 'Compiles top posts from a specified subreddit for a specified time period into a well-formatted Kindle book.',
-    long_description = """\
-                       See http://www.github.com/Antrikshy/reddit2Kindle for instructions. Requires KindleGen\
-                       from Amazon to convert HTML result to .mobi for Kindle.
-                       """,
-    classifiers = ['Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities',
-                   'Development Status :: 4 - Beta',
-                   'Intended Audience :: End Users/Desktop',
-                   'Natural Language :: English',
-                   'License :: OSI Approved :: MIT License'],
+setup(
+    name='reddit2Kindle',
+    scripts=['r2K'],
+    packages=['redditKindleLib'],
+    version='0.5.0',
+    author='Antriksh Yadav',
+    author_email='antrikshy@gmail.com',
+    url='http://www.antrikshy.com/projects/reddit2Kindle.htm',
+    description=(
+        'Compiles top posts from a specified subreddit for a specified time'
+        'period into a well-formatted Kindle book.'
+    ),
+    long_description=(
+        'See http://www.github.com/Antrikshy/reddit2Kindle for instructions.'
+        'Requires KindleGen from Amazon to convert HTML result to .mobi for'
+        'Kindle.'
+    ),
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License'
+    ],
 )
